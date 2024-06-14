88d1015c546b32667affe22625ab1b53531182d9
Mireya Garcia <mireya.garcia@epn.edu.ec>
2024-06-14 12:55:24 -0500

    version3-García Mireya

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import sistemaTransporte.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        pasajeros pasajero = new pasajeros();
+        pasajeros pasajero2 = new pasajeros();
+        pasajeros pasajero3 = new pasajeros();
+        pasajeros pasajero4 = new pasajeros();
+        pasajeros pasajero5 = new pasajeros();
     }
 }
